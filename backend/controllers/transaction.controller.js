7e9f228a7e9a9d08687e244442b3fe71d2323558
agastya gaur <agastyagaur17@gmail.com>
2024-03-01 20:30:19 +0530

    Add transaction CRUD operations

--- backend/controllers/transaction.controller.js
+++ backend/controllers/transaction.controller.js
@@ -29,3 +29,71 @@ export const createTransaction = async (req, res) => {
         console.log(error);
     }
 }
+
+export const getTransaction = async (req, res) => {
+    try {
+        const transactions = await Transaction.find({user: req.user._id});
+        if(!transactions){
+            return res.status(400).json({message:"No transactions found"});
+        }
+        return res.status(200).json({transactions});
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const deleteTransaction = async (req, res) => {
+    
+    try {
+        const {id} = req.params;
+        const deletedTransaction = await Transaction.findByIdAndDelete(id);
+        if(!deletedTransaction){
+            return res.status(400).json({message:"Transaction not deleted"});
+        }
+        return res.status(200).json({message:"Transaction deleted successfully"});
+
+        
+
+      
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const updateTransaction = async (req, res) => {
+    const {id} = req.params;
+    const {description,amount,paymentType,category,location} = req.body;
+    
+    try {
+      const transaction = await Transaction.findById(req.params.id);
+        if(transaction){
+            transaction.description = description || transaction.description;
+            transaction.amount = amount || transaction.amount;
+            transaction.paymentType = paymentType || transaction.paymentType;
+            transaction.category = category || transaction.category;
+            transaction.location = location || transaction.location;
+            const updatedTransaction = await transaction.save({validateBeforeSave:false});
+            if(updatedTransaction){
+                return res.status(200).json({message:"Transaction updated successfully",updatedTransaction});
+            }
+        }else{
+            return res.status(400).json({message:"Transaction not found"});
+        }
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const getTransactionById = async (req, res) => {
+    const {id} = req.params;
+    try {
+        const transaction = await Transaction.findById(id);
+        if(!transaction){
+            return res.status(400).json({message:"Transaction not found"});
+        }
+        return res.status(200).json({transaction});
+    } catch (error) {
+        console.log(error);
+    }
+}
--- backend/routes/transaction.route.js
+++ backend/routes/transaction.route.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { createTransaction } from "../controllers/transaction.controller.js";
+import { createTransaction, deleteTransaction, getTransaction, getTransactionById, updateTransaction } from "../controllers/transaction.controller.js";
 import { verifyUser } from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
 router.post('/create',verifyUser,createTransaction)
+router.get('/get-all',verifyUser,getTransaction)
+router.delete('/delete/:id',verifyUser,deleteTransaction)
+router.put('/update/:id',verifyUser,updateTransaction)
+router.get('/get-id/:id',verifyUser,getTransactionById)
 
 
 export default router;
