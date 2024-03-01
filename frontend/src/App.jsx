558f1660d90386262acc2330e6dbe3ce39d5ee56
agastya gaur <agastyagaur17@gmail.com>
2024-03-01 20:58:39 +0530

    Add frontend files and configurations

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className=' underline'>
+      hello
+    </div>
+  )
+}
+
+export default App
