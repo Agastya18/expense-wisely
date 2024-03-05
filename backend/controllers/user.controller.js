91389a9648b565a4d75a0626ef09cc41999be5f4
agastya gaur <agastyagaur17@gmail.com>
2024-03-05 13:30:19 +0530

    Fix import case in user.controller.js

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,4 +1,4 @@
-import User from "../models/user.Model.js";
+import User from "../models/user.model.js";
 
 export const SignUp = async (req, res) => {
     const {  name:fullName, username, password,gender} = req.body
